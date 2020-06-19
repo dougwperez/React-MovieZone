897cc43918ddcd436dbed4afbb4bfd9909b99315
Douglas Perez <douglasperez@Douglass-MacBook-Pro.local>
2020-06-19 17:22:57 +0800

    Updates

---
+++ precache-manifest.ee9bdf141946e787d85800f61c9d2cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3670616a3abb29f1b8dd28c97a9bf8c2",
+    "url": "/React-MovieZone/index.html"
+  },
+  {
+    "revision": "6b6bd549557807dea8eb",
+    "url": "/React-MovieZone/static/css/main.3cab559b.chunk.css"
+  },
+  {
+    "revision": "ce496520192a08805f78",
+    "url": "/React-MovieZone/static/js/2.cb8492dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-MovieZone/static/js/2.cb8492dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6bd549557807dea8eb",
+    "url": "/React-MovieZone/static/js/main.7605a442.chunk.js"
+  },
+  {
+    "revision": "bfb9f45b7eb1dd0a4d9c",
+    "url": "/React-MovieZone/static/js/runtime-main.6ba42ba0.js"
+  }
+]);
